27b7a66bdc024a81103738df1015d22666993c7f
Vhodnoylogin <kill.spama@gmail.com>
2022-09-25 14:52:32 +0300

    кажется, решили задачу

--- src/main/java/study/java_lessons/lesson_2_4/Work.java
+++ src/main/java/study/java_lessons/lesson_2_4/Work.java
@@ -3,14 +3,14 @@ package study.java_lessons.lesson_2_4;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Work {
 
     protected static final String SEP = ":";
+    protected static final String END_LINE = "\n";
+
 
     public static void main(String[] args) {
 //        System.out.println("TEST");
@@ -19,7 +19,8 @@ public class Work {
         String path = "E:\\IdeaProjects\\Stepik\\src\\main\\resources\\source.txt";
         try {
             Text text = new Text(path);
-            ps.println(printTextPerRole(text.getRoles(), text.getTextLines()));
+            String res = printTextPerRole(text.getRoles(), text.getTextLines());
+            ps.println(res);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -29,25 +30,28 @@ public class Work {
     protected static PrintStream ps = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 
     protected static String printTextPerRole(String[] roles, String[] textLines) {
-        Map<String, List<String>> roleText = new HashMap<>();
+        Map<String, StringBuilder> roleText = new HashMap<>();
         for (String role : roles) {
-            roleText.put(role + SEP, new ArrayList<>(10));
+            roleText.put(role + SEP, new StringBuilder());
         }
         for (int i = 0; i < textLines.length; i++) {
             String line = textLines[i];
             findRole(line, roleText, i + 1);
         }
-        ps.println(roleText);
-        return "";
+        StringBuilder res = new StringBuilder();
+        for (String role : roles) {
+            StringBuilder string = roleText.get(role + SEP);
+            res.append(role).append(SEP).append(END_LINE).append(string).append(END_LINE);
+        }
+        return res.toString().trim();
     }
 
-    protected static void findRole(String text, Map<String, List<String>> roleText, int num) {
+    protected static void findRole(String text, Map<String, StringBuilder> roleText, int num) {
         int index = text.indexOf(SEP) + 1;
         String role = text.substring(0, index);
         text = text.substring(index);
-//        List<String> texts = roleText.getOrDefault(role, null);
-        List<String> texts = roleText.get(role);
-        if (texts != null) texts.add(num + ")" + text);
+        StringBuilder texts = roleText.get(role);
+        if (texts != null) texts.append(num).append(")").append(text).append(END_LINE);
     }
 
 }
