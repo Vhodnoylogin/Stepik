7d1282b085f00a9529073af2bc749b8a9cf64217
Vhodnoylogin <kill.spama@gmail.com>
2022-09-25 17:24:05 +0300

    идея сама за меня решила задачу, сгенерировав нужный код.

---
+++ src/main/java/study/java_lessons/lesson_3_4/Work.java
@@ -0,0 +1,4 @@
+package study.java_lessons.lesson_3_4;
+
+public class Work {
+}
--- src/test/java/test/TestMath.java
+++ src/test/java/test/TestMath.java
@@ -1,45 +1,24 @@
 package test;
 
-
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import study.java_lessons.lesson_3_3.DirectionExt;
+import org.junit.jupiter.api.Test;
+import study.java_lessons.lesson_3_4.ComplexNumber;
 
 public class TestMath {
 
     @Test
-    public void testTurnLeft_UP() {
-        DirectionExt dir = DirectionExt.UP;
-
-        Assertions.assertSame(dir.turnLeft(), DirectionExt.LEFT);
-        Assertions.assertSame(dir.turnLeft().turnLeft(), DirectionExt.DOWN);
-        Assertions.assertSame(dir.turnLeft().turnLeft().turnLeft(), DirectionExt.RIGHT);
-        Assertions.assertSame(dir.turnLeft().turnLeft().turnLeft().turnLeft(), DirectionExt.UP);
-    }
-
-    @Test
-    public void testTurnRight_UP() {
-        DirectionExt dir = DirectionExt.UP;
-
-        Assertions.assertSame(dir.turnRight(), DirectionExt.RIGHT);
-        Assertions.assertSame(dir.turnRight().turnRight(), DirectionExt.DOWN);
-        Assertions.assertSame(dir.turnRight().turnRight().turnRight(), DirectionExt.LEFT);
-        Assertions.assertSame(dir.turnRight().turnRight().turnRight().turnRight(), DirectionExt.UP);
-    }
-
-    @Test
-    public void testMathCos() {
-        Assertions.assertSame(DirectionExt.movX(DirectionExt.UP), 0);
-        Assertions.assertSame(DirectionExt.movX(DirectionExt.LEFT), -1);
-        Assertions.assertSame(DirectionExt.movX(DirectionExt.DOWN), 0);
-        Assertions.assertSame(DirectionExt.movX(DirectionExt.RIGHT), 1);
-    }
-
-    @Test
-    public void testMathSin() {
-        Assertions.assertSame(DirectionExt.movY(DirectionExt.UP), 1);
-        Assertions.assertSame(DirectionExt.movY(DirectionExt.LEFT), 0);
-        Assertions.assertSame(DirectionExt.movY(DirectionExt.DOWN), -1);
-        Assertions.assertSame(DirectionExt.movY(DirectionExt.RIGHT), 0);
+    public void testComp() {
+        ComplexNumber cn1 = new ComplexNumber(2.3412, 7.2434);
+        ComplexNumber cn2 = new ComplexNumber(2.3412, 7.2434);
+        ComplexNumber cn3 = new ComplexNumber(3.23124, 1435.23);
+        Assertions.assertEquals(cn1, cn2);
+        Assertions.assertEquals(cn2, cn1);
+        Assertions.assertNotEquals(cn2, cn3);
+        Assertions.assertNotEquals(cn3, cn1);
+        Assertions.assertEquals(cn1, cn1);
+        Assertions.assertEquals(cn1.hashCode(), cn2.hashCode());
+        Assertions.assertEquals(cn2.hashCode(), cn1.hashCode());
+        Assertions.assertNotEquals(cn3.hashCode(), cn1.hashCode());
+        Assertions.assertNotEquals(cn3.hashCode(), cn2.hashCode());
     }
 }
