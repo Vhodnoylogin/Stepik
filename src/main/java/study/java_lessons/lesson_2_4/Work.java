f4724eae2a37b0874c48ec45ac7d915710f1466a
Vhodnoylogin <kill.spama@gmail.com>
2022-09-25 14:39:42 +0300

    сумел разбить в мап

--- src/main/java/study/java_lessons/lesson_2_4/Work.java
+++ src/main/java/study/java_lessons/lesson_2_4/Work.java
@@ -3,8 +3,15 @@ package study.java_lessons.lesson_2_4;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Work {
+
+    protected static final String SEP = ":";
+
     public static void main(String[] args) {
 //        System.out.println("TEST");
 
@@ -12,7 +19,6 @@ public class Work {
         String path = "E:\\IdeaProjects\\Stepik\\src\\main\\resources\\source.txt";
         try {
             Text text = new Text(path);
-            PrintStream ps = new PrintStream(System.out, true, StandardCharsets.UTF_8);
             ps.println(printTextPerRole(text.getRoles(), text.getTextLines()));
 
         } catch (IOException e) {
@@ -20,9 +26,28 @@ public class Work {
         }
     }
 
+    protected static PrintStream ps = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+
     protected static String printTextPerRole(String[] roles, String[] textLines) {
+        Map<String, List<String>> roleText = new HashMap<>();
+        for (String role : roles) {
+            roleText.put(role + SEP, new ArrayList<>(10));
+        }
+        for (int i = 0; i < textLines.length; i++) {
+            String line = textLines[i];
+            findRole(line, roleText, i + 1);
+        }
+        ps.println(roleText);
         return "";
     }
 
+    protected static void findRole(String text, Map<String, List<String>> roleText, int num) {
+        int index = text.indexOf(SEP) + 1;
+        String role = text.substring(0, index);
+        text = text.substring(index);
+//        List<String> texts = roleText.getOrDefault(role, null);
+        List<String> texts = roleText.get(role);
+        if (texts != null) texts.add(num + ")" + text);
+    }
 
 }
