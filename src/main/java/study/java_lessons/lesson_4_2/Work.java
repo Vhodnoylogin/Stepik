f19443cd62e1c08db1ad91e509f86da3bbf11529
Vhodnoylogin <kill.spama@gmail.com>
2022-09-26 22:36:27 +0300

    исправил ошибку Метод close() все же бросает исключение. Поэтому его надо тоже проверять.

--- src/main/java/study/java_lessons/lesson_4_2/Work.java
+++ src/main/java/study/java_lessons/lesson_4_2/Work.java
@@ -1,25 +1,31 @@
 package study.java_lessons.lesson_4_2;
 
 public class Work {
+    public static int MAX = 10;
+    public static int MIN = 1;
+
+    public static int ERR_PROB = 5;
+    public static int TOTAL_ERR_PROB = 9;
+
     public static void main(String[] args) {
         try {
             moveRobot(() -> {
                 int rand = random();
-                if (rand > 9) throw new RuntimeException("Runtime Connection error");
-                if (rand > 5) throw new RobotConnectionException("Connection error");
+                if (rand > TOTAL_ERR_PROB) throw new RuntimeException("Runtime Connection error");
+                if (rand > ERR_PROB) throw new RobotConnectionException("Connection error");
                 return new RobotConnection() {
                     @Override
                     public void moveRobotTo(int x, int y) {
                         int rand = random();
-                        if (rand > 9) throw new RuntimeException("Runtime Move error");
-                        if (rand > 5) throw new RobotConnectionException("Move error");
+                        if (rand > TOTAL_ERR_PROB) throw new RuntimeException("Runtime Move error");
+                        if (rand > ERR_PROB) throw new RobotConnectionException("Move error");
                     }
 
                     @Override
                     public void close() {
-//                    int rand = random();
-//                    if (rand > 9) throw new RuntimeException("Runtime Close error");
-//                    if (rand > 5) throw new RobotConnectionException("Close error");
+                        int rand = random();
+//                    if (rand > TOTAL_ERR_PROB) throw new RuntimeException("Runtime Close error");
+                        if (rand > ERR_PROB) throw new RobotConnectionException("Close error");
                     }
                 };
             }, 5, 7);
@@ -29,14 +35,15 @@ public class Work {
     }
 
     public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
-        for (int i = 1; i <= 3; i++) {
+        boolean flag = false;
+        for (int i = 1; i <= 3 && !flag; i++) {
             try (RobotConnection conn = robotConnectionManager.getConnection()) {
                 conn.moveRobotTo(toX, toY);
+                flag = true;
                 System.out.println("All right at " + i + " attempt");
-                break;
             } catch (RobotConnectionException e) {
 //                System.out.println(e.getMessage());
-                if (i == 3) throw e;
+                if (i == 3 && !flag) throw e;
             } catch (Exception e) {
                 System.out.println("Crash at " + i + " attempt with " + e.getMessage());
                 throw e;
@@ -45,8 +52,6 @@ public class Work {
     }
 
     public static int random() {
-        int MAX = 10;
-        int MIN = 1;
         return (int) Math.floor(Math.random() * (MAX - MIN + 1) + MIN);
     }
 }
