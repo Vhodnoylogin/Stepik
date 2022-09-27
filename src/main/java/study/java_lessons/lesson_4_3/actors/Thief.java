94d4c1e5177458506c04d0f216b06ae7c08b2637
Vhodnoylogin <kill.spama@gmail.com>
2022-09-27 12:32:12 +0300

    complete Thief

--- src/main/java/study/java_lessons/lesson_4_3/actors/Thief.java
+++ src/main/java/study/java_lessons/lesson_4_3/actors/Thief.java
@@ -1,11 +1,34 @@
 package study.java_lessons.lesson_4_3.actors;
 
-import study.java_lessons.lesson_4_3.help.MailService;
-import study.java_lessons.lesson_4_3.help.Sendable;
+import study.java_lessons.lesson_4_3.help.Package;
+import study.java_lessons.lesson_4_3.help.mail.MailPackage;
+import study.java_lessons.lesson_4_3.help.mail.Sendable;
+import study.java_lessons.lesson_4_3.help.service.MailService;
 
 public class Thief implements MailService {
+    protected static final String THIEF_MSG = "stones instead of {content}";
+    protected int minValueToStole;
+    protected int totalStolenAmount = 0;
+
+    public Thief(int minValueToStole) {
+        this.minValueToStole = minValueToStole;
+    }
+
+    public int getStolenValue() {
+        return totalStolenAmount;
+    }
+
     @Override
     public Sendable processMail(Sendable mail) {
-        return null;
+        if (!(mail instanceof MailPackage)) return mail;
+        Package pack = ((MailPackage) mail).getContent();
+        if (pack.price() < this.minValueToStole) return mail;
+
+        totalStolenAmount += pack.price();
+        pack = new Package(
+                String.format(THIEF_MSG.replace("{content}", pack.content()))
+                , 0
+        );
+        return new MailPackage(mail.getFrom(), mail.getTo(), pack);
     }
 }
