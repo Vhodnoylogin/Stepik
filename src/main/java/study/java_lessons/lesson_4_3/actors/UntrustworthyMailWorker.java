92cee611b234feec13435639b79264613f451fa5
Vhodnoylogin <kill.spama@gmail.com>
2022-09-27 13:20:54 +0300

    complete UntrustworthyMailWorker

--- src/main/java/study/java_lessons/lesson_4_3/actors/UntrustworthyMailWorker.java
+++ src/main/java/study/java_lessons/lesson_4_3/actors/UntrustworthyMailWorker.java
@@ -1,11 +1,28 @@
 package study.java_lessons.lesson_4_3.actors;
 
-import study.java_lessons.lesson_4_3.help.MailService;
-import study.java_lessons.lesson_4_3.help.Sendable;
+import study.java_lessons.lesson_4_3.help.mail.Sendable;
+import study.java_lessons.lesson_4_3.help.service.MailService;
+import study.java_lessons.lesson_4_3.help.service.RealMailService;
 
 public class UntrustworthyMailWorker implements MailService {
+
+    protected final MailService realMailService = new RealMailService();
+    protected MailService[] nodes;
+
+    public UntrustworthyMailWorker(MailService[] nodes) {
+        this.nodes = nodes;
+    }
+
+    public MailService getRealMailService() {
+        return realMailService;
+    }
+
     @Override
     public Sendable processMail(Sendable mail) {
-        return null;
+        Sendable m = mail;
+        for (MailService node : this.nodes) {
+            m = node.processMail(m);
+        }
+        return this.realMailService.processMail(m);
     }
 }
