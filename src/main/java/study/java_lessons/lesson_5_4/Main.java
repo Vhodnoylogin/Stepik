8fd043a7bc3ad70a7a582999ff77b5a57177e0a0
Vhodnoylogin <kill.spama@gmail.com>
2022-09-29 02:51:19 +0300

    prepare to solve task

---
+++ src/main/java/study/java_lessons/lesson_5_4/Main.java
@@ -0,0 +1,12 @@
+package study.java_lessons.lesson_5_4;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static Animal[] deserializeAnimalArray(byte[] data) throws IllegalArgumentException {
+        // your implementation here
+        throw new IllegalArgumentException();
+    }
+}
