77ea1d21f7c57c3614e9f35a8942991abb938d3a
Vhodnoylogin <kill.spama@gmail.com>
2022-09-30 14:24:05 +0300

    test of access

---
+++ src/main/java/study/java_lessons/lesson_6_2/Main.java
@@ -0,0 +1,12 @@
+package study.java_lessons.lesson_6_2;
+
+public class Main {
+    public static void main(String[] args) {
+//        Collection<?> collection = new ArrayList<>();
+//        Object o = new String("q");
+//
+//        collection.addAll(Collections.singletonList(o));
+    }
+
+
+}
